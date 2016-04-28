327b7555c4d6ffb5642e2d59d009c1000a550c0b
leowang <buaawangdong@163.com>
2016-04-28 16:25:08 +0800

    add a function file2matrix

--- kNN.py
+++ kNN.py
@@ -1,5 +1,12 @@
+#!usr/bin/python
+#-*- coding:UTF-8-*-
+
 from numpy import *
 import operator
+import sys
+		
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 def createDataSet():
 	group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
@@ -18,3 +25,18 @@ def classify0(inX, dataSet, labels, k):
 		classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
 	sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter(1), reverse = True)
 	return sortedClassCount[0][0]
+
+def file2matrix(filename):
+	fr = open(filename)
+	arrayOLines = fr.readlines()
+	numberOfLines = len(arrayOLines)
+	returnMat = zeros((numberOfLines, 3))
+	classLabelVector = []
+	index = 0
+	for line in arrayOLines:
+		line = line.strip()   
+		listFromLine = line.split('\t')
+		returnMat[index, :] = listFromLine[0 : 3]
+		classLabelVector.append(listFromLine[-1])
+		index += 1
+	return returnMat, classLabelVector
