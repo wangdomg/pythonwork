bcbf2f13561dfdf67fb138f42b08edc6ac0fbc48
leowang <buaawangdong@163.com>
2016-04-28 17:25:18 +0800

    finish the kNN for dating website

--- kNN.py
+++ kNN.py
@@ -12,6 +12,7 @@ def createDataSet():
 	group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
 	labels = ['A', 'A', 'B', 'B']
 	return group, labels
+
 def classify0(inX, dataSet, labels, k):
 	dataSetSize = dataSet.shape[0]
 	diffMat = tile(inX, (dataSetSize, 1)) - dataSet
@@ -40,3 +41,38 @@ def file2matrix(filename):
 		classLabelVector.append(listFromLine[-1])
 		index += 1
 	return returnMat, classLabelVector
+
+def autoNorm(dataSet):
+	minVals = dataSet.min(0)
+	maxVals = dataSet.max(0)
+	ranges = maxVals - minVals
+	normDataSet = zeros(shape(dataSet))
+	m = dataSet.shape[0]
+	normDataSet = dataSet - tile(minVals, (m, 1))
+	normDataSet = normDataSet / tile(ranges, (m, 1))
+	return normDataSet, ranges, minVals
+
+def datingClassTest():
+	hoRatio = 0.10
+	datingDataMat, datingDataLabels = file2matrix('../datingTestSet.txt')
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	m = normMat.shape[0]
+	numTestVecs = int(m * hoRatio)
+	errorCount = 0.0
+	for i in range(numTestVecs):
+		classifierResult = classify0(normMat[i, :], normMat[numTestVecs : m, :], datingDataLabels[numTestVecs : m], 3)
+		print "the classifier came back with : %d, the real answer is : %d" % (classifierResult, datingDataLabels[i])
+		if(classifierResult != datingDataLabels[i]):
+			errorCount += 1.0
+	print "the total error rate is : %f" % (errorCount / float(numTestVecs))
+
+def classifyPerson():
+	resultList = ['not at all', 'in small doses', 'in large doses']
+	percentTats = float(raw_input("percentage of time spent playing video games?"))
+	ffMiles = float(raw_input("frequent flier miles earned per year?"))
+	iceCream = float(raw_input("liters of ice cream consumed per year?"))
+	datingDataMat, datingLabels = file2matrix('../datingTestSet.txt')
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	inArr = array([ffMiles, percentTats, iceCream])
+	classifierResult = classify0((inArr - minVals) / ranges, normMat, datingLabels, 3)
+	print "You will probably like this person : " resultList[classifierResult - 1]
